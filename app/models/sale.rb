f96dc20b866bb72891eb4ede822c52cc02993c76
Bihos <sageoffroy@gmail.com>
2019-10-31 08:56:36 -0300

    traducción de atributos

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,3 +1,8 @@
 class Sale < ApplicationRecord
   belongs_to :client
+  has_many :sale_details, inverse_of: :sale
+  accepts_nested_attributes_for :sale_details, reject_if: :all_blank, allow_destroy: true
+
+  
+
 end
