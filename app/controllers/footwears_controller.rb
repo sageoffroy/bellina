e3e21ae8c2527f1a74ec6b67fec0d80ce1ea0c79
Silvio Geoffroy <sageoffroy@gmail.com>
2020-04-23 09:58:27 -0300

    Prueba de tamaño pdf

--- app/controllers/footwears_controller.rb
+++ app/controllers/footwears_controller.rb
@@ -18,7 +18,7 @@ class FootwearsController < ApplicationController
     respond_to do |format|
       format.html
       format.json
-      format.pdf {render template:'footwears/template', pdf:'Nombre', page_size: nil, page_width:'0.8in', page_height:'1.5in', margin:{top:'0.1in', bottom:'0.1in',left:'0.1in',right:'0.1in'}}
+      format.pdf {render template:'footwears/template', pdf:'Nombre', page_size: nil, page_width:'1.2in', page_height:'1.2in', margin:{top:'0.1in', bottom:'0.1in',left:'0.1in',right:'0.1in'}}
     end
   end
 
