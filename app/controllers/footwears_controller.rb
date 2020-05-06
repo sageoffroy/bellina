4c499caa1488c77adeef355dd88eb1bec86cba9b
Silvio Geoffroy <sageoffroy@gmail.com>
2020-05-06 08:37:23 -0300

    barcode

--- app/controllers/footwears_controller.rb
+++ app/controllers/footwears_controller.rb
@@ -18,7 +18,7 @@ class FootwearsController < ApplicationController
     respond_to do |format|
       format.html
       format.json
-      format.pdf {render template:'footwears/template', pdf:'Nombre', page_size: nil, page_width:'1.8in', page_height:'1.30in', margin:{top:'0.01in', bottom:'0.01in',left:'0.01in',right:'0.01in'}}
+      format.pdf {render template:'footwears/template', pdf:'Nombre', page_size: nil, page_width:'4in', page_height:'2in', margin:{top:'0.03in', bottom:'0.01in',left:'0.01in',right:'0.01in'}}
     end
   end
 
