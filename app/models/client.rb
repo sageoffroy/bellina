fa4f1692aaecebe0a92e2cf6775db9d365f4c6a5
SILVIO GEOFFROY <sageoffroy@gmail.com>
2020-10-29 18:05:32 -0300

    Corrección en Monto Anterior, valor positivo

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,19 +4,19 @@ class Client < ApplicationRecord
 	has_many :payments
 
 	def to_s
-		if dni.nil? or dni.blank?	
+		if dni.nil? or dni.blank?
 			fullname
 		else
 			fullname + "(" + dni.to_s + ")"
 		end
 	end
 
-	
+
 	def get_debt
 		amount = 0
 
 		if !amount_old.nil?
-			amount = amount_old
+			amount = -amount_old
 		end
 
 		sales.each do |sale|
